1e3c846d5863b11d5fe4acfcf9c2f8c31ec91850
BautistaMalu <89524797+BautistaMalu@users.noreply.github.com>
2022-05-06 14:25:59 -0300

    instatietor,while y alg mas
    lesto

---
+++ Assets/Instantiator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiator : MonoBehaviour
+{
+    public GameObject objectToClone;
+
+    public void hola()
+    {
+        int count = 0;
+        while (count < 3)
+        {
+            Instantiate(objectToClone);
+            count++;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
